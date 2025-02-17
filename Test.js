a798ef5fcc75cab2cdffe26b75463fcb02b21fbc
sohaila <souhailanachafi@gmail.com>
2025-02-17 09:18:54 +0100

    Fix ESLint errors in Test.js

--- Test.js
+++ Test.js
@@ -1,4 +1,9 @@
 console.log('Test');
-const foo = "bar"; // Unused variable (if rule is configured)
-foo = 42; // Readonly assignment to constant (if rule is configured)
 
+// Declare foo only once
+let foo = "bar";  // This can be reassigned if needed
+console.log(foo);
+
+// If you need to assign a new value, just reassign it instead of redeclaring
+foo = 42;  // Reassign the value to the existing foo
+console.log(foo);
